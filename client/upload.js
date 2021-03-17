4411767b2152d6d4b91b9733689169226bbca970
mabroor007 <amabroor007@gmail.com>
2021-03-18 00:28:23 +0500

    added upload progress to the client

--- client/upload.js
+++ client/upload.js
@@ -1,6 +1,10 @@
 const uploadBtn = document.getElementById("upload-btn");
 
+const progress = document.getElementById("progress");
+
 const uploadFile = async (file) => {
+  progress.innerText = "";
+
   try {
     // I haveA
     console.log(`${file.name} : ${file.size}bytes : ${file.type}`);
@@ -8,12 +12,22 @@ const uploadFile = async (file) => {
     const formdata = new FormData();
     formdata.append("file", file);
     formdata.append("filename", file.name);
-    const response = await fetch("http://localhost:5000/mabroor", {
-      method: "POST",
-      body: formdata,
+
+    const response = await axios.request({
+      method: "post",
+      url: "http://localhost:5000/mabroor",
+      data: formdata,
+      onUploadProgress: (p) => {
+        console.log(p);
+        progress.innerText = p.loaded / p.total;
+      },
     });
-    const res = await response.json()
-    console.log(res)
+
+    // Setting the progress because upload completed
+    progress.innerText = "100%";
+
+    const res = await response.json();
+    console.log(res);
   } catch (e) {
     console.log(e);
   }
